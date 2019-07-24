5b4b1f9f224b1ed743a567f6e66f458da9f42eaf
dungld96 <dunglvh96@gmail.com>
2019-07-24 17:40:29 +0700

    update

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -1,7 +1,7 @@
 
 @extends('layout.client.client')
 @section('content')
-<div class="container">
+<div class="container content-change-pass">
 	<div class="row">
 		<div class="col-md-offset-3 col-md-6 col-sm-offset-3 col-sm-6">
 			<div class="form_container">
@@ -9,15 +9,15 @@
                     {{csrf_field()}}
 					<h2>Đổi mật khẩu</h2>
 					<div class="form-group">
-						<div class="input-group">
+						<div class="input-group margin-top-m">
 							<span class="input-group-addon"><i class="fas fa-phone-alt fa-fw"></i></span>
 							<input type="password" name="old_password" class="form-control input-lg" placeholder="Mật khẩu hiện tại">
 						</div>
-						<div class="input-group">
+						<div class="input-group margin-top-m">
 							<span class="input-group-addon"><i class="fas fa-phone-alt fa-fw"></i></span>
                             <input type="password" name="password" class="form-control input-lg" placeholder="Mật khẩu mới" >
 						</div>
-						<div class="input-group">
+						<div class="input-group margin-top-m">
 							<span class="input-group-addon"><i class="fas fa-phone-alt fa-fw"></i></span>
                             <input type="password" name="password_confirm" class="form-control input-lg" placeholder="Xác nhận mật khẩu mới">
 						</div>
