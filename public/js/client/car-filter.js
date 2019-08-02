6e79bf3e4e17d0953c10144726aca1b9216fffcd
dungld96 <dunglvh96@gmail.com>
2019-08-02 17:34:48 +0700

    update

--- public/js/client/car-filter.js
+++ public/js/client/car-filter.js
@@ -1,5 +1,79 @@
 $(document).ready(function() {
     
+    var date = moment();
+    let startTime = "00:00:00";
+    let endTime = "00:00:00";
+    let startDate = $.query.get('startDate');
+    let endDate = $.query.get('endDate');
+    let address = $.query.get('address');
+
+
+    if(address){
+        $('input[name=place]').val(address);
+    }
+
+    if(startDate){
+        $('.start_date span').html(moment.unix(startDate).format('DD/MM/YYYY'));
+        $('.start_date .time').val(moment.unix(startDate).format('HH:mm:ss'));
+    }
+
+    if(endDate){
+        $('.end_date span').html(moment.unix(endDate).format('DD/MM/YYYY'));
+        $('.end_date .time').val(moment.unix(endDate).format('HH:mm:ss'));
+    }
+
+    function start_date_change(date) {
+        $('.start_date span').html(date.format('DD/MM/YYYY'));
+        $('.end_date span').html(date.format('DD/MM/YYYY'));
+        startDate = date.format('DD/MM/YYYY');
+        endDate = date.format('DD/MM/YYYY');
+    }
+
+    function end_date_change(date) {
+        $('.end_date span').html(date.format('DD/MM/YYYY'));
+        endDate = date.format('DD/MM/YYYY');
+    }
+
+    $('.start_date .date').daterangepicker({
+        singleDatePicker: true,
+        minDate: new Date()
+    }, start_date_change);
+
+    $('.end_date .date').daterangepicker({
+        singleDatePicker: true,
+        minDate: new Date()
+    }, end_date_change);
+
+    $('.start_date span').on('apply.daterangepicker', function (ev, picker) {
+        $('.end_date span').daterangepicker({
+            locale: { "format": "DD/MM/YYYY" },
+            singleDatePicker: true,
+            minDate: $('.start_date span').text()
+        }, end_date_change);
+    });
+
+    $('input[name=place]').on('focus', function () {
+        $(this).val('');
+    });
+
+    $('input[name=place]').on('input', function () {
+        var text = $(this).val();
+        if (text == '') {
+            $('.place_result').remove();
+            return;
+        }
+        placesSuggest(text);
+    });
+
+    $('.start_date .time').on('change', function() {
+        startTime = this.value;
+    });
+
+    $('.end_date .time').on('change', function() {
+        endTime = this.value;
+    });
+
+
     $('body').on('change', '#filterCarSortBy', function (e) {
         let sortby = this.value;
         if(sortby){
--- resources/views/client/car/filter-result.blade.php
+++ resources/views/client/car/filter-result.blade.php
@@ -5,7 +5,53 @@
         <div class="container">
             <div class="result-header">
                 <div class="tab-mode">
-                    <p class="list-result-title"> <i class="far fa-list-alt"></i>Danh sách</p>
+                    <p class="list-result-title"> <i class="far fa-list-alt"></i><span> Danh sách</span></p>
+                </div>
+                <div id="mb-address-daterange">
+                    <div class="mb-content-address-daterange">
+                        <p id="mb-plade">Hà Nội, Việt Nam</p>
+                        <p id="mb-date-range">
+                            <span id="mb-start-date">10/07/2019 </span><span id="mb-start-time">00:00</span> -
+                            <span id="mb-end-date">10/07/2019 </span><span id="mb-end-time">00:00</span>
+                        </p>
+                    </div>
+                    <div class="mb-icon-dropdow">
+                            <i class="fas fa-angle-down"></i>
+                    </div>
+                </div>
+                <div class="daterange" id="adress-daterange">
+                    <div class="form-inline select-datetime select-address">
+                        <label>Địa điểm</label>
+                        <div class="place_wrap input-group">
+                             <input type="text" name="place" placeholder="Nhập địa điểm...">
+                        </div>
+                    </div>
+                    <div class="form-inline select-datetime">
+                        <label>Bắt đầu</label>
+                        <div class="input-group select-date start_date">
+                            <span class="date">10/07/2019</span>
+                            <i class="fas fa-angle-down"></i>
+                        </div>
+                        <div class="input-group start_date">
+                            <select class="time">
+                                <option value="00:00:00">00:00</option><option value="00:30:00">00:30</option><option value="01:00:00">01:00</option><option value="01:30:00">01:30</option><option value="02:00:00">02:00</option><option value="02:30:00">02:30</option><option value="03:00:00">03:00</option><option value="03:30:00">03:30</option><option value="04:00:00">04:00</option><option value="04:30:00">04:30</option><option value="05:00:00">05:00</option><option value="05:30:00">05:30</option><option value="06:00:00">06:00</option><option value="06:30:00">06:30</option><option value="07:00:00">07:00</option><option value="07:30:00">07:30</option><option value="08:00:00">08:00</option><option value="08:30:00">08:30</option><option value="09:00:00">09:00</option><option value="09:30:00">09:30</option><option value="10:00:00">10:00</option><option value="10:30:00">10:30</option><option value="11:00:00">11:00</option><option value="11:30:00">11:30</option><option value="12:00:00">12:00</option><option value="12:30:00">12:30</option><option value="13:00:00">13:00</option><option value="13:30:00">13:30</option><option value="14:00:00">14:00</option><option value="14:30:00">14:30</option><option value="15:00:00">15:00</option><option value="15:30:00">15:30</option><option value="16:00:00">16:00</option><option value="16:30:00">16:30</option><option value="17:00:00">17:00</option><option value="17:30:00">17:30</option><option value="18:00:00">18:00</option><option value="18:30:00">18:30</option><option value="19:00:00">19:00</option><option value="19:30:00">19:30</option><option value="20:00:00">20:00</option><option value="20:30:00">20:30</option><option value="21:00:00">21:00</option><option value="21:30:00">21:30</option><option value="22:00:00">22:00</option><option value="22:30:00">22:30</option><option value="23:00:00">23:00</option><option value="23:30:00">23:30</option>                        </select>
+                            <i class="fas fa-angle-down"></i>
+                        </div>
+                    </div>
+                    <div id="mb-g">-</div>
+                    <div class="form-inline select-datetime">
+                        <label>Kết thúc</label>
+                        <div class="input-group select-date end_date">
+                            <span class="date">10/07/2019</span>
+                            <i class="fas fa-angle-down"></i>
+                        </div>
+                        <div class="input-group end_date">
+                                <select class="time">
+                                    <option value="00:00:00">00:00</option><option value="00:30:00">00:30</option><option value="01:00:00">01:00</option><option value="01:30:00">01:30</option><option value="02:00:00">02:00</option><option value="02:30:00">02:30</option><option value="03:00:00">03:00</option><option value="03:30:00">03:30</option><option value="04:00:00">04:00</option><option value="04:30:00">04:30</option><option value="05:00:00">05:00</option><option value="05:30:00">05:30</option><option value="06:00:00">06:00</option><option value="06:30:00">06:30</option><option value="07:00:00">07:00</option><option value="07:30:00">07:30</option><option value="08:00:00">08:00</option><option value="08:30:00">08:30</option><option value="09:00:00">09:00</option><option value="09:30:00">09:30</option><option value="10:00:00">10:00</option><option value="10:30:00">10:30</option><option value="11:00:00">11:00</option><option value="11:30:00">11:30</option><option value="12:00:00">12:00</option><option value="12:30:00">12:30</option><option value="13:00:00">13:00</option><option value="13:30:00">13:30</option><option value="14:00:00">14:00</option><option value="14:30:00">14:30</option><option value="15:00:00">15:00</option><option value="15:30:00">15:30</option><option value="16:00:00">16:00</option><option value="16:30:00">16:30</option><option value="17:00:00">17:00</option><option value="17:30:00">17:30</option><option value="18:00:00">18:00</option><option value="18:30:00">18:30</option><option value="19:00:00">19:00</option><option value="19:30:00">19:30</option><option value="20:00:00">20:00</option><option value="20:30:00">20:30</option><option value="21:00:00">21:00</option><option value="21:30:00">21:30</option><option value="22:00:00">22:00</option><option value="22:30:00">22:30</option><option value="23:00:00">23:00</option><option value="23:30:00">23:30</option>                        </select>
+                                <i class="fas fa-angle-down"></i>
+                        </div>
+                        <div class="error error_end_date text-danger hidden"><i class="fas fa-exclamation-circle"></i> Vui lòng chọn thời gian kết thúc!</div>
+                    </div>
                 </div>
             </div>
             <div class="content-result">
